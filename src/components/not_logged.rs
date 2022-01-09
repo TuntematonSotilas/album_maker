988c183a3ef828c61e9345dcdfd6558286a99365
TuntematonSotilas <batault_gael@msn.com>
2022-01-09 12:22:01 +0100

    design page

--- src/components/not_logged.rs
+++ src/components/not_logged.rs
@@ -1,11 +1,18 @@
 use seed::{prelude::*, *};
 
 pub fn view<Ms>() -> Node<Ms> {
-	div![C!["hero", "is-medium"],
+	div![C!["hero", "is-large", "columns", "is-flex", "is-vcentered"],
         div![C!["hero-body"],
-            h1![C!["title"],
-                "Please log in to continue",
-            ],
-        ]
+			section![
+				span![C!["icon", "is-size-1", "has-text-info"],
+					i![C!("ion-log-in")]
+				],
+			],
+			section![
+				h1![C!["title"],
+					"Please log in to continue",
+				],
+			],
+        ],
     ]
 }
