e0e5a512f7beb00fb5e5888b487eccce34e572aa
TuntematonSotilas <batault_gael@msn.com>
2022-05-22 11:17:58 +0200

    clippy

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 //  - it's useful when you want to check your code with `cargo make verify`
 // but some rules are too "annoying" or are not applicable for your case.)
 #![allow(clippy::wildcard_imports)]
+#![allow(clippy::unused_unit)]
 
 use crate::components::*;
 use components::notification::NotifType;
