2eee2d89310cac2926241e7a401ae9de51006f2e
TuntematonSotilas <batault_gael@msn.com>
2023-01-18 13:44:20 +0100

    bckg

--- src/models/vars.rs
+++ src/models/vars.rs
@@ -3,4 +3,4 @@ pub const UPLOAD_URI: &str = "https://api.cloudinary.com/v1_1/gatoorg/image/uplo
 pub const DESTROY_URI: &str = "https://api.cloudinary.com/v1_1/gatoorg/image/destroy";
 pub const THUMB_URI: &str = "https://res.cloudinary.com/gatoorg/image/upload/w_128,h_128,c_fill/";
 pub const IMG_URI: &str = "https://res.cloudinary.com/gatoorg/image/upload/";
-pub const VERY_LOW_URI: &str = "https://res.cloudinary.com/gatoorg/image/upload/q_auto:low,w_60/";
+pub const VERY_LOW_URI: &str = "https://res.cloudinary.com/gatoorg/image/upload/q_auto:low,w_5,h_1/";
